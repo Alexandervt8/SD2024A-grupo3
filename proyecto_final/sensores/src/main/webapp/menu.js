37828f0d7c890c3e3a41bae4da5c221806938280
Alexandervt8 <145701447+Alexandervt8@users.noreply.github.com>
2024-07-21 17:15:04 -0500

    Version 5
    Correccion de tildes en htmls
    Vista de graficos
    Funcionalidad en tiempo real de vista agicultor y vista sensores

--- proyecto_final/sensores/src/main/webapp/menu.js
+++ proyecto_final/sensores/src/main/webapp/menu.js
@@ -1,13 +1,15 @@
 // menu.js
 document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.getElementById('sidebar');
-    sidebar.innerHTML = `
+    sidebar.innerHTML =
+    `
         <h3>Menú</h3>
         <ul>
+            <li><a href="index.html" class="menu-link">Inicio</a></li>
             <li><a href="cliente_a.html" class="menu-link">Agricultor</a></li>
-            <li><a href="cliente_e.html" class="menu-link">Estacion</a></li>
-            <li><a href="configura_variable.html" class="menu-link">Configurar Variables</a></li>
-            <!-- Añade más enlaces según sea necesario -->
+            <li><a href="cliente_e.html" class="menu-link">Datos de sensores</a></li>
+            <li><a href="vista_graficos.jsp" class="menu-link">Monitor</a></li>
+            <li><a href="configura_variable.html" class="menu-link">Configuración</a></li>
         </ul>
     `;
 });
